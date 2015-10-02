7655023f0b12cceb361b51f811056e350507e2ee
DanielArF <daniel_af96@hotmail.com>
2015-10-02 15:00:16 +0200

    WIP add weather and wind servies for three cities

--- src/app.js
+++ src/app.js
@@ -5,8 +5,26 @@ function updateDisplay() {
     $('#temperature').css('color', thermostat.colorStatus());
 }
 
+function getWeather(location, callback) {
+    var api_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric';
+    $.ajax({
+        dataType: "jsonp",
+        url: api_url,
+        success: callback
+    });
+}
+
+function getWeatherForSelectedCity(location) {
+    getWeather(location, function (response) {
+        $('#city').html(response.name);
+        $('#city_temp').html(response.main.temp);
+        $('#city_wind').html(response.wind.speed);
+    });
+}
+
 $(document).ready(function () {
     updateDisplay();
+    getWeatherForSelectedCity($('#current_city').val());
 
     $('#increase').click(function () {
         thermostat.increaseTemperature(1);
@@ -33,24 +51,4 @@ $(document).ready(function () {
     $('#current_city').change(function () {
         getWeatherForSelectedCity($(this).val());
     });
-
-
 });
-
-function getWeather(location, callback) {
-
-    var api_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric';
-
-    $.ajax({
-        dataType: "jsonp",
-        url: api_url,
-        success: callback
-    });
-}
-
-function getWeatherForSelectedCity(location) {
-    getWeather(location, function (response) {
-        $('#city').html(response.name);
-        $('#city_temp').html(response.main.temp);
-    });
-}
